327f5866e69f41ab0022942d371ed5c02b811b54
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-06-23 00:42:28 +0800

    32

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,7 +13,7 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     // wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    // wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
      wsPath: import.meta.env.VITE_REVERB_PATH,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws'],
