299d1b1d46689af2ca57aea1619633174de78e09
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-06-23 12:50:38 +0800

    jj

--- resources/js/server_reverb.js
+++ resources/js/server_reverb.js
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-=======
-import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
@@ -8,10 +5,12 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    // wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    // wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+     wsPath: import.meta.env.VITE_REVERB_PATH,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws'],
+    // enabledTransports: ['ws', 'wss'],
 });
 
 // Log when WebSocket connection is established
@@ -23,4 +22,5 @@ window.Echo.connector.pusher.connection.bind('connected', () => {
 window.Echo.connector.pusher.connection.bind('error', (error) => {
     console.error('WebSocket connection error:', error);
 });
->>>>>>> Stashed changes
+
+
