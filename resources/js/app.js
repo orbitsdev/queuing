957f15b5c239f991fb16fe910e7d7f46f9235bee
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-07-27 21:33:59 +0800

    updae for revervis

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,19 +8,29 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//      wsPath: import.meta.env.VITE_REVERB_PATH,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws'],
+//     // enabledTransports: ['ws', 'wss'],
+// });
+
+// Log when WebSocket connection is established
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-     wsPath: import.meta.env.VITE_REVERB_PATH,
+    wsPath: import.meta.env.VITE_REVERB_PATH,  // ✅ forced proxy path `/reverb/`
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws'],
-    // enabledTransports: ['ws', 'wss'],
 });
 
-// Log when WebSocket connection is established
 window.Echo.connector.pusher.connection.bind('connected', () => {
     console.log('WebSocket connection established successfully!');
 });
