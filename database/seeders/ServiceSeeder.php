131f94d4f2ccaf65344cac726bcff96a50a4fb87
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-08-11 09:38:47 +0800

    update file

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -16,8 +16,27 @@ class ServiceSeeder extends Seeder
          $branch = Branch::first();
 
         $services = [
-            ['name' => 'Cashier Payment', 'code' => 'C', 'description' => 'Payments and billing services'],
-            ['name' => 'Information Desk', 'code' => 'I', 'description' => 'Customer help and support'],
+            // Business
+            ['name' => 'Business Online Billing and Payment', 'code' => 'BOBP', 'description' => 'Business'],
+            ['name' => 'New Business Application', 'code' => 'NBA', 'description' => 'Business'],
+            ['name' => 'Renew Business Application', 'code' => 'RBA', 'description' => 'Business'],
+
+            // Real Property
+            ['name' => 'Realty Tax Online Billing and Payment', 'code' => 'RTBP', 'description' => 'Real Property'],
+
+            // Payment Order
+            ['name' => 'Online Payment Order', 'code' => 'OPO', 'description' => 'Payment Order'],
+
+            // Building and Construction
+            ['name' => 'Building Permit Requirements', 'code' => 'BPR', 'description' => 'Building and Construction'],
+            ['name' => 'Certificate of Occupancy Requirements', 'code' => 'COR', 'description' => 'Building and Construction'],
+            ['name' => 'Application Tracking', 'code' => 'AT', 'description' => 'Building and Construction'],
+            ['name' => 'Building Permit Application', 'code' => 'BPA', 'description' => 'Building and Construction'],
+            ['name' => 'OSCP Online Billing and Payment', 'code' => 'OSCP', 'description' => 'Building and Construction'],
+            ['name' => 'Certificate of Occupancy Application', 'code' => 'COA', 'description' => 'Building and Construction'],
+            ['name' => 'Pay PTR (Professional Tax Receipt)', 'code' => 'PPTR', 'description' => 'Building and Construction'],
+            ['name' => 'Register Professional', 'code' => 'RP', 'description' => 'Building and Construction'],
+            ['name' => 'Update Professional', 'code' => 'UP', 'description' => 'Building and Construction'],
         ];
 
         foreach ($services as $svc) {
